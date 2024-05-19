896c92ccf98db26a127c0ebd8499b9c0b40458a8
Rachel Parsons <143442234+parsonsrachel@users.noreply.github.com>
2024-05-18 22:34:06 -0400

    Update align_tes.sh

--- align_tes.sh
+++ align_tes.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#create a mafft alignment of all the L1MC1 TEs
 
 ../../../group/software/mafft-7.487-with-extensions/install/bin/mafft-linsi ../data/L1MC1_LINE.fa > ../data/L1MC1_alignment.aln
 
